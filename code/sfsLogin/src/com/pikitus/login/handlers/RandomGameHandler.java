253ea1071c7454ab57d101b5aed1c93334fcf349
ben <ben@localhost>
2010-12-14 18:00:32 -0400

    Added create room

--- code/sfsLogin/src/com/pikitus/login/handlers/RandomGameHandler.java
+++ code/sfsLogin/src/com/pikitus/login/handlers/RandomGameHandler.java
@@ -1,37 +1,31 @@
 package com.pikitus.login.handlers;
 
-public class RandomGameHandler 
+import com.smartfoxserver.v2.api.CreateRoomSettings;
+import com.smartfoxserver.v2.entities.Room;
+import com.smartfoxserver.v2.entities.User;
+import com.smartfoxserver.v2.entities.Zone;
+import com.smartfoxserver.v2.entities.data.ISFSObject;
+import com.smartfoxserver.v2.exceptions.SFSCreateRoomException;
+import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
+
+public class RandomGameHandler extends BaseClientRequestHandler
 {
-/*	public class SchedulerTest extends SFSExtension
-	{
-	    private class TaskRunner implements Runnable
-	    {
-	        private int runningCycles = 0;
-	         
-	        public void run()
-	        {
-	            runningCycles++;    
-	            trace("Inside the running task. Cycle:  " + runningCycles);
-	             
-	            if (runningCycles >= 10)
-	            {
-	                trace("Time to stop the task!");
-	                taskHandle.cancel();
-	            }
-	        }
-	    }
-	     
-	    // Keeps a reference to the task execution
-	    ScheduledFuture<?> taskHandle;
-	     
-	    @Override
-	    public void init()
-	    {
-	        SmartFoxServer sfs = SmartFoxServer.getInstance();
-	         
-	        // Schedule the task to run every second, with no initial delay
-	        taskHandle = sfs.getTaskScheduler().scheduleAtFixedRate(new TaskRunner(), 0, 1, TimeUnit.SECONDS);
-	    }
-	}
-	*/
+    @Override
+    public void handleClientRequest(User user, ISFSObject params)
+    {
+        Zone zone = getParentExtension().getParentZone();
+        CreateRoomSettings room = new CreateRoomSettings();
+        boolean joinIt = true; 
+        Room roomToLeave = user.getLastJoinedRoom();
+        boolean fireClientEvent = true; 
+        boolean fireServerEvent = true;
+        try 
+        {
+			getApi().createRoom(zone, room, user, joinIt, roomToLeave , fireClientEvent, fireServerEvent);
+		} 
+        catch (SFSCreateRoomException e) 
+		{
+			e.printStackTrace();
+		}
+    }
 }
