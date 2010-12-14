c8d858c1ab0e1bc863fc2a4aa9d4f58d02301e12
ben <ben@localhost>
2010-12-14 17:21:49 -0400

    Added a custom login and added debugging to the startup script

--- code/sfsLogin/src/com/pikitus/login/SFSLogin.java
+++ code/sfsLogin/src/com/pikitus/login/SFSLogin.java
@@ -4,7 +4,8 @@ import com.smartfoxserver.v2.core.SFSEventType;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
 import com.smartfoxserver.v2.extensions.SFSExtension;
-import com.pikitus.login.OnUserGoneHandler;
+import com.pikitus.login.handlers.LoginEventHandler;
+import com.pikitus.login.handlers.OnUserGoneHandler;
 
 public class SFSLogin extends SFSExtension 
 {
@@ -14,10 +15,14 @@ public class SFSLogin extends SFSExtension
 	public void init() {
 	
 		trace("SFSLogin Extension started, rel. " + version);
-	    addRequestHandler("getFriends", FriendsList.class);
-	    
+		
+		// Standard events
+		addEventHandler(SFSEventType.USER_LOGIN, LoginEventHandler.class); 
 	    addEventHandler(SFSEventType.USER_DISCONNECT, OnUserGoneHandler.class);
 	    addEventHandler(SFSEventType.USER_LEAVE_ROOM, OnUserGoneHandler.class);
+
+	    // Get facebook friends
+	    addRequestHandler("getFriends", FriendsList.class);
 	}
 
 	public void sendSFSObject(String name, ISFSObject object)
